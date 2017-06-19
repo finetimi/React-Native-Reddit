42cccb697e0356e54707a6487e565215b9bd4e38
Felix Inetimi <f.inetimi@yahoo.com>
2017-06-19 03:22:18 -0400

    Axios config

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -1,5 +1,6 @@
 /* constatns that can be used anywhere in the app,
 all put under one section to prevent collision */
+import axios from 'axios';
 
 // API constants
 export const APP_ID 			= '327TeSVrOAFLXQ';
@@ -8,3 +9,11 @@ export const LOGIN_URI 			= `https://www.reddit.com/api/v1/authorize.compact?cli
 // Redux Types
 export const AUTH_USER 			= 'AUTH_USER';
 export const FETCHING_USER_DATA = 'FETCHING_USER_DATA';
+
+// Thunk to return axios config
+export const AXIOS 	= (token)=> {
+	return axios.create({
+		baseURL: 'https://oath.reditt.com',
+		headers: {'Authorization': `bearer ${token}`}
+	});
+}
