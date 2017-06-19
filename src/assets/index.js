0c9e3bfa73682dbfb6916f71eb42d13e329c3ccb
Felix Inetimi <f.inetimi@yahoo.com>
2017-06-19 03:19:21 -0400

    built login container webview

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 const images = {
 	redditImage: require('./images/redditimage.png'),
+	backgroundImage: require('./images/redditbgd.jpg'),
+	redditChat: require('./images/redditchat.png'),
 }
 
 export default images;
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Images from '../assets';
+import {
+	View,
+	ToucableHighlight,
+	Image,
+	StyleSheets
+} from 'react-native';
