9592ab6b7c23884e4011887f0b3265ee427e61c5
finetimi <finetimi@mediweightloss.com>
2017-07-02 19:20:55 -0400

    Fixed constants spacing descripancy

--- src/actions/constants.js
+++ src/actions/constants.js
@@ -3,13 +3,13 @@ all put under one section to prevent collision */
 import axios from 'axios';
 
 // API constants
-export const APP_ID 			= '327TeSVrOAFLXQ';
-export const LOGIN_URI 			= `https://www.reddit.com/api/v1/authorize.compact?client_id=${APP_ID}&response_type=token&state=RANDOM_STRING&redirect_uri=rnreddit://redirectURI&scope=identity%20edit%20flair%20history%20modconfig%20modflair%20modlog%20modposts%20modwiki%20mysubreddits%20privatemessages%20read%20report%20save%20submit%20subscribe%20vote%20wikiedit%20wikiread`;
+export const APP_ID 	= '327TeSVrOAFLXQ';
+export const LOGIN_URI 	= `https://www.reddit.com/api/v1/authorize.compact?client_id=${APP_ID}&response_type=token&state=RANDOM_STRING&redirect_uri=rnreddit://redirectURI&scope=identity%20edit%20flair%20history%20modconfig%20modflair%20modlog%20modposts%20modwiki%20mysubreddits%20privatemessages%20read%20report%20save%20submit%20subscribe%20vote%20wikiedit%20wikiread`;
 
 // Redux Types
-export const AUTH_USER 			= 'AUTH_USER';
+export const AUTH_USER 		= 'AUTH_USER';
 export const FETCHING_USER_DATA = 'FETCHING_USER_DATA';
-export const LOADING_POSTS		= 'LOADING_POSTS';
+export const LOADING_POSTS	= 'LOADING_POSTS';
 export const STORE_HOT_POSTS 	= 'STORE_HOT_POSTS';
 export const STORE_RANDOM_POSTS = 'STORE_RANDOM_POSTS';
 export const STORE_SUBREDDITS	= 'STORE_SUBREDDITS';
